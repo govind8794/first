3e8210a910e68774c9ef7f069f10516c69e84b32
Dinesh <dineshu07@gmail.com>
2019-09-11 01:13:43 +0530

    adding code

---
+++ hello.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+export default class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>         
+        Hello Next.js
+      </div>
+    )
+  }
+}
